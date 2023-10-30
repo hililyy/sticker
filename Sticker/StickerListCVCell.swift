afe69e1ae151d87657ccb75fa4e685b8609c718c
hililyy <joun406@gmail.com>
2023-10-30 18:39:39 +0900

    [FEAT] 사진에 아이콘 추가

---
+++ Sticker/StickerListCVCell.swift
@@ -0,0 +1,46 @@
+//
+//  StickerListCVCell.swift
+//  Sticker
+//
+//  Created by 강조은 on 2023/10/30.
+//
+
+import UIKit
+
+final class StickerListCVCell: UICollectionViewCell {
+    
+    static let identifier = "StickerListCVCell"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initSubviews()
+        initConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initSubviews() {
+        contentView.addSubview(imageView)
+    }
+    
+    func initConstraints() {
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
+    }
+}
---
+++ Sticker/StickerView.swift
@@ -0,0 +1,58 @@
+//
+//  StickerView.swift
+//  Sticker
+//
+//  Created by 강조은 on 2023/10/30.
+//
+
+import UIKit
+
+class StickerView: UIView {
+    var contentsView = UIView()
+    var parentVC: UIViewController?
+    
+    convenience init(contentsView: UIView, frame: CGRect) {
+        self.init(frame: frame)
+        self.contentsView = contentsView
+        initalize()
+        initConstraints()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initalize() {
+        setGesture()
+        contentsView.isUserInteractionEnabled = true
+    }
+    
+    func setGesture() {
+        let panGesture = UIPanGestureRecognizer.init(target: self, action: #selector(drag))
+        self.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func drag(sender: UIPanGestureRecognizer) {
+        let parent = parentVC as! ViewController
+        let translation = sender.translation(in: parent.backgroundImageView)
+        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+        sender.setTranslation(.zero, in: self)
+    }
+    
+    func initConstraints() {
+        addSubview(contentsView)
+        
+        contentsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentsView.topAnchor.constraint(equalTo: topAnchor),
+            contentsView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentsView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentsView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
